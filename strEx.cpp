a99546f8f0a65b4900bdb4d71568662fb3c5dd75
ChuckMizess <62080916+ChuckMizess@users.noreply.github.com>
2020-03-15 02:31:59 +0300

    Create strEx.cpp
    Calculate the Length: strlen()

---
+++ strEx.cpp
@@ -0,0 +1,19 @@
+//Calculate the Length: strlen()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) 
+{
+    /* Exit if no second argument is found. */
+    if (argc != 2) 
+    {
+        puts("Argument missing.");
+        return EXIT_FAILURE;
+    }
+
+    size_t len = strlen(argv[1]);
+    printf("The length of the second argument is %zu.\n", len);
+
+    return EXIT_SUCCESS;
+}
